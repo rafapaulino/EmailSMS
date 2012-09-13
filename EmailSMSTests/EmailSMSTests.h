4d44c6361cd26db9db83977d199fa338cc3f8a63
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-13 09:29:14 -0300

    Initial Commit

---
+++ EmailSMSTests/EmailSMSTests.h
@@ -0,0 +1,13 @@
+//
+//  EmailSMSTests.h
+//  EmailSMSTests
+//
+//  Created by Rafael Brigagão Paulino on 13/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface EmailSMSTests : SenTestCase
+
+@end
